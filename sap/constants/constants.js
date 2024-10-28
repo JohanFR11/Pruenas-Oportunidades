01183f55bb4e9727569afada92bed64421ba3449
CamiloSTDev <camitotena@gmail.com>
2024-10-28 16:54:32 -0500

    modificacion en las credenciales y link de la odata

--- sap/constants/constants.js
+++ sap/constants/constants.js
@@ -1,12 +1,7 @@
 import 'dotenv/config'
 
 export const CREDENTIALS = {
-  url: process.env.SAP_URL_PRODUCCTION,
-  auth: {
-    username: process.env.SAP_USERNAME_PRODUCTION,
-    password: process.env.SAP_PASSWORD_PRODUCTION
-  },
-  urlTest: process.env.SAP_URL_TEST,
+  url: process.env.SAP_URL_TEST,
   authTest: {
     username: process.env.SAP_USERNAME_TEST,
     password: process.env.SAP_PASSWORD_TEST
